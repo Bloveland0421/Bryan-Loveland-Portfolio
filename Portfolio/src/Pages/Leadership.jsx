80130bf18919a2db5593e6d3d80fe4082f1138db
Bloveland0421 <lovelandbryan486@gmail.com>
2025-02-19 10:18:38 -0500

    leadership-page

--- Portfolio/src/Pages/Leadership.jsx
+++ Portfolio/src/Pages/Leadership.jsx
@@ -12,8 +12,11 @@ const Leadership = () => {
 
     return (
         <div className="leadership-container">
-            <h1>Leadership Page</h1>
-            <p>This is the leadership page content.</p>
+            <div className='leadership-content'>
+                 <h1 className='leadership-header-styling'>Leadership</h1>
+            <p className='leadership-body-styling'>People benefit from my leadership by gaining clarity, confidence, and the resources they need to succeed. I prioritize open communication, active listening, and personalized support, ensuring that each individual feels heard and valued. By fostering a collaborative environment, I empower others to take ownership of their roles while offering guidance when needed. My focus on setting clear goals, providing constructive feedback, and celebrating achievements helps build trust and motivates others to reach their full potential. Through my leadership, people develop both personally and professionally, growing in their skills and confidence, which leads to higher team morale and overall success.</p>
+            </div>
+            <img className='leadership-image' src="/public/leadership3.png" alt="" />
         </div>
     );
 }
